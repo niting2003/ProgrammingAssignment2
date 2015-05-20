91e9695566704019a8d2b24f003d0542124d87f0
niting2003 <nitin.gupta2342003@gmail.com>
2015-05-20 14:13:29 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,6 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+## R programming assignment-2 Lexical Scoping: To Cache the inverse of a matrix.
 
 ## Write a short comment describing this function
 
